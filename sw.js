f412cdc4e8cbebbe297198ba3d8d8ebef0cf199e
Kyle Hunkler <khunkler@arthrex.com>
2026-07-09 21:35:49 -0400

    Pool expedition collision buckets

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // SmackTown service worker: precache the whole app so it launches instantly
 // and works offline (solo mode fully offline; multiplayer needs a network).
-const CACHE = 'smacktown-v98';
+const CACHE = 'smacktown-v99';
 
 const ASSETS = [
   './',
