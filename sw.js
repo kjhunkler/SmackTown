c4b4cc410884d0db6f52d728f5c0cbbaa1ace784
Kyle Hunkler <khunkler@arthrex.com>
2026-07-09 22:23:01 -0400

    Show version beside game title

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // SmackTown service worker: precache the whole app so it launches instantly
 // and works offline (solo mode fully offline; multiplayer needs a network).
-const CACHE = 'smacktown-v108';
+const CACHE = 'smacktown-v109';
 
 const ASSETS = [
   './',
